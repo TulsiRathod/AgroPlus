7d421cf6c935f2004fc8d69c04dec40b61f002f8
TulsiRathod <tulsirathod9102@gmail.com>
2024-08-11 04:56:57 +0530

    change url

--- FrontEnd/src/components/AgroLogin.jsx
+++ FrontEnd/src/components/AgroLogin.jsx
@@ -11,7 +11,7 @@ const AgroLogin = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:3000/api/userLogin', {
+            const response = await fetch('http://localhost:8000/api/userLogin', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- FrontEnd/src/components/AgroRegister.jsx
+++ FrontEnd/src/components/AgroRegister.jsx
@@ -19,7 +19,7 @@ const SignupForm = () => {
         setError(null);
 
         try {
-            const response = await fetch('http://localhost:3000/api/register', {
+            const response = await fetch('http://localhost:8000/api/register', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
